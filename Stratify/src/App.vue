d7ea76a2b89a2346b868602dc15134bdf8f4076b
Gilvane Amaro <gilvane.amaro.jr@gmail.com>
2025-03-06 21:18:14 -0300

    initial commit

---
+++ Stratify/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+      </nav>
+    </div>
+  </header>
+
+
+</template>
+
+<style scoped>
+
+</style>
