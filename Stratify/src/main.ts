b922642fbb750b36240d0f1898e1dea2024ed284
santosjhony12 <santosdesouzajhony@gmail.com>
2025-03-11 22:19:52 -0300

    feat (interface): adicionado navbar e configurado router

--- Stratify/src/main.ts
+++ Stratify/src/main.ts
@@ -2,12 +2,24 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import PrimeVue, { defaultOptions } from 'primevue/config';
 import App from './App.vue'
+import Aura from '@primeuix/themes/aura';
+import { Menubar } from 'primevue';
+import router from './router';
 
 
 const app = createApp(App)
-
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            darkModeSelector: false
+        }
+    }
+});
+app.use(router);
+app.component('Menubar', Menubar);
 app.use(createPinia())
 
 
---
+++ Stratify/src/views/DashboardView.vue
@@ -0,0 +1,31 @@
+
+<template>
+    <div class="card">
+        <Menubar :model="items"/>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from "vue";
+  import { Menubar } from "primevue";
+  const items = ref([
+    {
+        label: 'Home',
+        icon: 'pi pi-home'
+    },
+    {
+        label: 'Projects',
+        icon: 'pi pi-briefcase',
+    },
+    {
+        label: 'Time', 
+        icon: 'pi pi-users'
+    },
+    {
+        label: 'Sair',
+        icon: 'pi pi-sign-out'
+    }
+  ]);
+  </script>
+  <style>
+  </style>
