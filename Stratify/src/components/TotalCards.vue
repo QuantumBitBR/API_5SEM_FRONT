fe79d3ba0bbb0fe15d2d534846189776255895bf
Caio Sousa <caio.sousa5@fatec.sp.gov.br>
2025-03-15 23:05:02 -0300

    feat(dashboard): add TotalCards component and added axios package

---
+++ Stratify/src/components/TotalCards.vue
@@ -0,0 +1,44 @@
+<template>
+  <Card class="p-4 text-white custom-card" id="totalCards">
+    <template #content>
+      <div class="text-sm">Cards totais</div>
+      <div class="text-3xl font-bold">{{ totalCards }}</div>
+    </template>
+  </Card>
+</template>
+
+<script>
+import axios from 'axios';
+import Card from 'primevue/card';
+
+export default {
+  components: { Card },
+  data() {
+    return {
+      totalCards: '---',
+    };
+  },
+  methods: {
+    async fetchTotalCards() {
+      try {
+        const response = await axios.get(''); // API endpoint
+        this.totalCards = response.data.total || '---';
+      } catch (error) {
+        console.error('Erro ao buscar os dados:', error);
+        this.totalCards = '---';
+      }
+    },
+  },
+  mounted() {
+    this.fetchTotalCards();
+  },
+};
+</script>
+
+<style scoped>
+.custom-card {
+  background: linear-gradient(135deg, #5aa17e, #468f66);
+  border-radius: 8px;
+  color: white;
+}
+</style>
--- Stratify/src/views/DashboardView.vue
+++ Stratify/src/views/DashboardView.vue
@@ -1,31 +1,26 @@
-
 <template>
-    <div class="card">
-        <Menubar :model="items"/>
-    </div>
-  </template>
-  
-  <script setup>
-  import { ref } from "vue";
-  import { Menubar } from "primevue";
-  const items = ref([
-    {
-        label: 'Home',
-        icon: 'pi pi-home'
-    },
-    {
-        label: 'Projects',
-        icon: 'pi pi-briefcase',
-    },
-    {
-        label: 'Time', 
-        icon: 'pi pi-users'
-    },
-    {
-        label: 'Sair',
-        icon: 'pi pi-sign-out'
-    }
-  ]);
-  </script>
-  <style>
-  </style>
+  <div class="card">
+    <Menubar :model="items" />
+    <TotalCards />
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import Menubar from "primevue/menubar";
+import TotalCards from "@/components/TotalCards.vue"; // Importando o componente do card
+
+const items = ref([
+  { label: "Home", icon: "pi pi-home" },
+  { label: "Projects", icon: "pi pi-briefcase" },
+  { label: "Time", icon: "pi pi-users" },
+  { label: "Sair", icon: "pi pi-sign-out" },
+]);
+</script>
+
+<style scoped>
+.card {
+  position: relative;
+}
+
+</style>
