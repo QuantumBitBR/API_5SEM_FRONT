72d99eabdbf4112188c55e10867fb3798de74b6c
AndreMeneses0103 <andremeneses0103@gmail.com>
2025-03-29 22:05:29 -0300

    SCRUM-43-refactor(logs): random logs removed

--- Stratify/src/services/lifeTimeService.ts
+++ Stratify/src/services/lifeTimeService.ts
@@ -2,10 +2,8 @@ import api from "./apiConfig";
 
 class LifeTimeService{
     async quantityPerProject(project_id: any){
-      console.log(project_id);
       try{
             let endpoint = ""
-          console.log(project_id.id);
             endpoint = `/fatoeficiencia/projeto/${project_id.id}`;
             const response = await api.get(endpoint);
             return response.data;
