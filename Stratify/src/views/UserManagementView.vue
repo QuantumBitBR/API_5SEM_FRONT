087f1c9a958d73198788807759ab03b89a31452e
AndreMeneses0103 <andremeneses0103@gmail.com>
2025-04-11 23:45:43 -0300

    SCRUM-54-feat(view): User Management View file added

---
+++ Stratify/src/views/UserManagementView.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="my-view">
+      <h1>{{ title }}</h1>
+      <p>{{ message }}</p>
+      <button @click="handleClick">Clique aqui</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'UserManagement',
+    data() {
+      return {
+        title: 'Minha View',
+        message: 'Olá'
+      };
+    },
+    methods: {
+      handleClick() {
+        this.message = 'Você clicou no botão!';
+      }
+    },
+    created() {
+      console.log('Componente criado');
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .my-view {
+    padding: 1rem;
+    font-family: Arial, sans-serif;
+  }
+  </style>
+  
