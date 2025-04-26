1bf39ea6617a031e133e6edbf6bedc3b6615235a
AndreMeneses0103 <andremeneses0103@gmail.com>
2025-04-25 21:43:32 -0300

    SCRUM-55-feat(Token): added token service with methods to manage authtoken

---
+++ Stratify/src/services/TokenService.ts
@@ -0,0 +1,30 @@
+import Cookies from "js-cookie";
+import { jwtDecode } from "jwt-decode";
+
+class TokenService {
+    private readonly tokenKey = "authToken";
+
+    public storeToken(token: string): void {
+        Cookies.set(this.tokenKey, token, { expires: 7 });
+    }
+
+    public getToken(): string | undefined {
+        return Cookies.get(this.tokenKey);
+    }
+
+    public removeToken(): void {
+        Cookies.remove(this.tokenKey);
+    }
+
+    public decodeToken(token?: string): any | null {
+        try {
+            const jwt = token || this.getToken();
+            return jwt ? jwtDecode(jwt) : null;
+        } catch (error) {
+            console.error("Error decoding token:", error);
+            return null;
+        }
+    }
+}
+
+export default new TokenService();
