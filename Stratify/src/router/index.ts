31ae6d07d07801791b900b367eafcb5ab4bf97fa
AndreMeneses0103 <andremeneses0103@gmail.com>
2025-05-17 22:13:15 -0300

    SCRUM-97-feat(block): restriction in /management added for admins

--- Stratify/src/router/index.ts
+++ Stratify/src/router/index.ts
@@ -1,4 +1,6 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import TokenService from '@/services/TokenService';
+import { createRouter, createWebHistory, type NavigationGuardNext, type RouteLocationNormalized } from 'vue-router';
+import { showToast } from '@/eventBus';
 
 const routes = [
     {
@@ -19,7 +21,39 @@ const routes = [
     {
         path: '/management',
         name: 'management',
-        component: () => import('../views/UserManagementView.vue')
+        component: () => import('../views/UserManagementView.vue'),
+        beforeEnter: (
+            to: RouteLocationNormalized,
+            from: RouteLocationNormalized,
+            next: NavigationGuardNext
+        ) => {
+            const token = TokenService.getToken();
+
+            if (!token) {
+                showToast({
+                    severity: 'error',
+                    summary: 'Session expired',
+                    detail: 'Your session has expired. Please, log in again',
+                    life: 3000
+                });
+                next('/');
+                return;
+            }
+
+            const role = TokenService.decodeToken(token)?.role;
+
+            if (role === 'ADMIN') {
+                next();
+            } else {
+                showToast({
+                    severity: 'error',
+                    summary: 'Unauthorized',
+                    detail: 'You are not allowed to access this page',
+                    life: 3000
+                });
+                next('/dashboard');
+            }
+        }
     },
     {
         path: '/reset',
