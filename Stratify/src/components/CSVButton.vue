06a585aa4f6f1cbded0e608308c768bcc5a77287
AndreMeneses0103 <andremeneses0103@gmail.com>
2025-05-23 21:32:30 -0300

    SCRUM-105-feat(button): created CSV button template

---
+++ Stratify/src/components/CSVButton.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <button @click="exportCSV">Export CSV</button>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        exportCSV() {
+            console.log('Exportando CSV...');
+        }
+    }
+}
+</script>
+
+<style>
+button {
+    padding: 8px 16px;
+    background-color: #4CAF50;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+
+button:hover {
+    background-color: #45a049;
+}
+</style>
