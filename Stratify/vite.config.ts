dc37ee010e5c42136b7a2e317d75de3ed16c3dcf
Caio Sousa <caio.sousa5@fatec.sp.gov.br>
2025-06-05 20:13:29 -0300

    feat(defined coverage)
    defined coverage tests for sonar cloud

--- Stratify/vite.config.ts
+++ Stratify/vite.config.ts
@@ -1,20 +1,29 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    tailwindcss()
+    tailwindcss(),
     // vueDevTools(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  test: {
+    // Ambiente para testes de componentes Vue
+    environment: 'jsdom',
+
+    // Configuração de relatório de cobertura
+    coverage: {
+      provider: 'v8',               // usa @vitest/coverage-v8
+      reporter: ['text', 'lcov'],   // 'lcov' é essencial para SonarCloud
+      reportsDirectory: './coverage',
     },
   },
 })
